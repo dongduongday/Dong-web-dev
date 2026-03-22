8eb86ce0d37b3fddec1a2adbb7716516b06ba891
donglearning-pro <dongduong28112008@gmail.com>
2026-03-22 15:37:17 +0700

    feat: new save function for passenger counter

--- script/pc.js
+++ script/pc.js
@@ -1,9 +1,16 @@
 let count = 0
 let countEl = document.getElementById("count-el")
+let errorEl = document.getElementById("error")
+
 function increment() {
-    count = ++count
+    count += 1
     countEl.innerText = count
 }
+
+let entriesEl = document.getElementById("entries-el")
+
 function save() {
-    console.log(count)
+    entriesEl.textContent += count + " - "
+    countEl.innerText = 0
+    count = 0
 }
