2079ceb04cdca08f3609b443f62aab10ae1b77b4
donglearning-pro <dongduong28112008@gmail.com>
2026-03-23 22:10:26 +0700

    feat: blackjack

---
+++ script/blackjack.js
@@ -0,0 +1,12 @@
+let firstCard = 13
+let secondCard = 9
+
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("Do you want draw a new card? 😐")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🤩")
+} else {
+    console.log("You're out of the game! 😭")
+}
