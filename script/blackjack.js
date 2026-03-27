b2ba6f1af7211275ef4d483992f0a4c31c9a8d63
donglearning-pro <dongduong28112008@gmail.com>
2026-03-27 22:04:42 +0700

    feat: boolean variable

--- script/blackjack.js
+++ script/blackjack.js
@@ -1,5 +1,7 @@
 let firstCard = 13
-let secondCard = 9
+let secondCard = 8
+let hasBlackJack = false
+let isAlive = true
 
 let sum = firstCard + secondCard
 
@@ -7,6 +9,11 @@ if (sum < 21) {
     console.log("Do you want draw a new card? 😐")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🤩")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
+    isAlive = false
 }
+
+console.log(hasBlackJack)
+console.log(isAlive)
