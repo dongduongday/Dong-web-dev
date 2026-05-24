2f430cc57a8658fd28fde9421076cbb958648d27
dongdev <dongduong28112008@gmail.com>
2026-05-24 07:19:26 +0700

    feat: new homepage with professional UI/UX

--- script/blackjack.js
+++ script/blackjack.js
@@ -5,14 +5,16 @@ let isAlive = true
 let sum = firstCard + secondCard
 let message = ""
 
-if (sum < 21) {
-    message = "Do you want draw a new card? 😐"
-} else if (sum === 21) {
-    hasBlackJack = true
-    message = "Wohoo! You've got Blackjack! 🤩"
-} else {
-    isAlive = false
-    message = "You're out of the game! 😭"
+function startGame() {
+    if (sum < 21) {
+        message = "Do you want draw a new card? 😐"
+    } else if (sum === 21) {
+        hasBlackJack = true
+        message = "Wohoo! You've got Blackjack! 🤩"
+    } else {
+        isAlive = false
+        message = "You're out of the game! 😭"
+    }
 }
 
 console.log(isAlive)
